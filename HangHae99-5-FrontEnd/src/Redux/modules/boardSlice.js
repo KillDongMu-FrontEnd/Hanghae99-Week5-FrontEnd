7660bc3d0aeb927c9d724d6bbfead6e3db7ef63f
kimsungho <ksh_1035@naver.com>
2022-10-22 21:13:36 +0900

    extra reducer 제외 및 원래 소희님 코드로 변경

--- HangHae99-5-FrontEnd/src/Redux/modules/boardSlice.js
+++ HangHae99-5-FrontEnd/src/Redux/modules/boardSlice.js
@@ -1,49 +1,37 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
-import{ postBoardApi, getBoardApi, delBoardApi, getBoardIdApi, editBoardApi } from "./boardApi"
+import{postBoardApi, getBoardApi, delBoardApi, getBoardIdApi, editBoardApi} from "./boardApi"
 
-
-const initialState = {
-  boards : [],
-  board: {},
-  isLoading: false,
-  error: null,
-}
 
 export const __postBoard = createAsyncThunk(
   "postBoard",
   async (payload, thunkAPI) => {
-    try{
-      await postBoardApi(payload);
-      return thunkAPI.dispatch(postBoard(payload))
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error);
-    }
+    await postBoardApi(payload);
+    thunkAPI.dispatch(postBoard(payload));
   }
 );
 
 export const __getBoard = createAsyncThunk(
   "getBoard",
   async (payload, thunkAPI) => {
-    const response = await axios.get(`http://localhost:3001/boardList`);
-    return thunkAPI.fulfillWithValue(response.data);
+    const response = await getBoardApi(payload);
+    thunkAPI.dispatch(getBoard(response));
   }
 );
   
 export const __delBoard = createAsyncThunk(
-  "delBoard",
-  async (payload, thunkAPI) => {
-    await delBoardApi(payload);
-    return thunkAPI.fulfillWithValue(payload);
-  }
-);
+    "delBoard",
+    async (payload, thunkAPI) => {
+      await delBoardApi(payload);
+      thunkAPI.dispatch(delBoard(payload));
+    }
+  );
 
 export const __getBoardId = createAsyncThunk(
   "getBoard_Id",
   async (payload, thunkAPI) => {
-    const response = await getBoardIdApi(`http://localhost:3001/boardList/${payload}`);
-    return thunkAPI.fulfillWithValue(response.data);
-}
+    const response = await getBoardIdApi(payload);
+    thunkAPI.dispatch(getBoard_Id(response));
+  }
 );
 
 export const __editBoard = createAsyncThunk(
@@ -56,96 +44,34 @@ export const __editBoard = createAsyncThunk(
 
 export const boardSlice = createSlice({
   name: "boards",
-  initialState,
-  reducers: {
-    // postBoard: (state, action) => {
-    //   const id = state.boards[state.boards.length - 1]?.id + 1 || 1;
-    //   state.boards.push({ id, ...action.payload });
-    // },
-    // getBoard: (state, action) => {
-    //   state.boards = action.payload;
-    // },
-    // delBoard: (state, action) => {
-    //   state.boards = action.payload.filter((item)=>item.id !== action.payload);
-    // },
-    // getBoard_Id: (state, action) => {
-    //   state.board = action.payload;
-    // },
-    // editBoard: (state, action) => {
-    //   state.boards = state.boards.map((board)=>{
-    //     return board.id === action.payload.id ? action.payload : board
-    //   })
-    // },
+  initialState:{
+      boards : [],
+      board: {},
+      isLoading: false,
+      error: null,    
   },
-  extraReducers: {
-    // GET request Reducers
-    [__getBoard.pending]: (state) => {
-      state.isLoading = true;
+  reducers: {
+    postBoard: (state, action) => {
+      const id = state.boards[state.boards.length - 1]?.id + 1 || 1;
+      state.boards.push({ id, ...action.payload });
     },
-    [__getBoard.fulfilled]: (state, action) => {
-      state.isLoading = false;
+    getBoard: (state, action) => {
       state.boards = action.payload;
     },
-    [__getBoard.rejected]: (state, action) => {
-      state.isLoading = false;
-      state.error = action.payload;;
-    },
-
-    // GET One BoardItem request Reducers
-    [__getBoardId.pending]: (state) => {
-      state.isLoading = true;
-    },
-    [__getBoardId.fulfilled]: (state, action) => {
-      state.isLoading = false;
-      state.board = action.payload;
-    },
-    [__getBoardId.rejected]: (state, action) => {
-      state.isLoading = false;
-      state.error = action.payload;;
-    },
-
-    // POST request Reducers
-    [__postBoard.pending]: (state) => {
-      state.isLoading = true;
-    },
-    [__postBoard.fulfilled]: (state, action) => {
-      state.isLoading = false;
-      state.boards = state.boards.push(action.payload);
-    },
-    [__postBoard.rejected]: (state, action) => {
-      state.isLoading = false;
-      state.error = action.payload
-    },
-
-    // DELETE request Reducers
-    [__delBoard.pending]: (state) => {
-      state.isLoading = true;
-    },
-    [__delBoard.fulfilled]: (state, action) => {
-      state.isLoading = false;
+    delBoard: (state, action) => {
       state.boards = action.payload.filter((item)=>item.id !== action.payload);
     },
-    [__delBoard.rejected]: (state, action) => {
-      state.isLoading = false;
-      state.error = action.payload
+    getBoard_Id: (state, action) => {
+      state.board = action.payload;
     },
-
-    // EDIT BoardItem request Reducers
-    [__editBoard.pending]: (state) => {
-      state.isLoading = true;
-    },
-    [__editBoard.fulfilled]: (state, action) => {
-      state.isLoading = false;
+    editBoard: (state, action) => {
       state.boards = state.boards.map((board)=>{
         return board.id === action.payload.id ? action.payload : board
       })
     },
-    [__editBoard.rejected]: (state, action) => {
-      state.isLoading = false;
-      state.error = action.payload;
-    }
-  }
+  },
+ 
 });
 
-export const { postBoard, getBoard, delBoard, getBoard_Id, editBoard } = boardSlice.actions;
+export const { postBoard, getBoard,delBoard,getBoard_Id,editBoard} = boardSlice.actions;
 export default boardSlice.reducer;
