a6d91913fed83ee79907648962f6a09861b68f3a
kimsungho <ksh_1035@naver.com>
2022-10-25 02:22:20 +0900

    로그인 상태에 따른 변화 체크 및 로그아웃 기능 , 글작성 페이지 구현 중

--- HangHae99-5-FrontEnd/src/components/Header/Header.jsx
+++ HangHae99-5-FrontEnd/src/components/Header/Header.jsx
@@ -1,6 +1,16 @@
 import { MenuToggle, Checkbox, Bar, Menu, MenuItem, MenuLink } from "./Header.styled"
+import { __logoutUser } from "../../Redux/modules/loginSlice"
+import { useDispatch } from "react-redux"
 
 export const Header = () => {
+
+  const userId = localStorage.getItem("username")
+  const dispatch = useDispatch();
+
+  // const logoutHandler = () => {
+  //   dispatch(__logoutUser());
+  // }
+
   return(
     <MenuToggle>
       <Checkbox type="checkbox" />
@@ -10,7 +20,13 @@ export const Header = () => {
       <Bar/>
 
       <Menu>
-        <MenuItem><MenuLink to="/login">Sign In<br/>(Log_out*)</MenuLink></MenuItem>
+        <MenuItem>
+          {
+            userId === null ? 
+            <MenuLink to="/login">Log In</MenuLink> : 
+            <p>Log Out</p>
+          }
+        </MenuItem>
         <hr/>
         <MenuItem><MenuLink to="/">Home</MenuLink></MenuItem>
         <MenuItem><MenuLink to="/form">글 작성하기*</MenuLink></MenuItem>
