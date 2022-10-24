b841989c14a6564f75447bfda8be29d18ed2a5cb
kimsungho <ksh_1035@naver.com>
2022-10-24 18:22:44 +0900

    회원가입(서버연동) 완료, extrareducer(X), 로그인 미구현

--- HangHae99-5-FrontEnd/src/Redux/modules/API/userAPI.js
+++ HangHae99-5-FrontEnd/src/Redux/modules/API/userAPI.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL = process.env.REACT_APP_SERVER
 
 export const postUserApi = async (payload) => {
-  console.log(payload)
+  console.log(payload);
   await axios.post(`${BASE_URL}/api/members/signup`, payload);
 };
 
