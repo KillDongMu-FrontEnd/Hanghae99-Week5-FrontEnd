1b74af2112e3b7d95f42b1e930a86592be6678f5
Limsohee <v_vsoi@naver.com>
2022-10-24 14:34:24 +0900

    파일업로드 추가_인풋만넣음

--- HangHae99-5-FrontEnd/src/components/Form.jsx
+++ HangHae99-5-FrontEnd/src/components/Form.jsx
@@ -1,62 +1,78 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { FormContainer, FormGroup } from "../style/Form.styled";
-import { FormInput, HighLight, InputBar, Label } from "../style/MaterialInput.styled";
+import axios from 'axios';
+
+import {
+  FormInput,
+  HighLight,
+  InputBar,
+  Label,
+} from "../style/MaterialInput.styled";
 import { __postBoard } from "../Redux/modules/boardSlice";
 
 export const Form = () => {
   const init = {
-      "title": "",
-      "content": "",
-      "username": "",
-      "createdAt": ""
-  }
+    title: "",
+    content: "",
+    username: "",
+    createdAt: "",
+  };
 
   const dispatch = useDispatch();
 
   const [input, setInput] = useState(init);
 
-
   //인풋밸류 input에 넣었음
-  const onChangeHandler = (e) =>{
-    const {name, value} = e.target;
-    setInput({...input, [name]: value });
-  }
+  const onChangeHandler = (e) => {
+    const { name, value } = e.target;
+    setInput({ ...input, [name]: value });
+  };
 
-  const onSubmitHandler = (e) =>{
+  const onSubmitHandler = (e) => {
     e.preventDefault();
     dispatch(__postBoard(input));
     setInput(init);
-  }
+  };
 
-  return(
+  return (
     <FormContainer onSubmit={onSubmitHandler}>
-        <FormGroup>
-          <FormInput
-            type="text"
-            autoComplete="off"
-            name="title"
-            value={input.title}
-            onChange={(e) => onChangeHandler(e)}
-          />
-          <HighLight/>
-          <InputBar/>
-          <Label>제목임</Label>
-        </FormGroup>
-        <FormGroup>
-          <FormInput 
-            type="text"
-            autoComplete="off"
-            name="content"
-            value={input.content}
-            onChange={(e) => onChangeHandler(e)} 
-          />
-          <HighLight/>
-          <InputBar/>
-          <Label>내용임</Label>
-        </FormGroup>
+      <FormGroup>
+        <FormInput
+          type="text"
+          autoComplete="off"
+          name="title"
+          value={input.title}
+          onChange={(e) => onChangeHandler(e)}
+        />
+        <HighLight />
+        <InputBar />
+        <Label>제목임</Label>
+      </FormGroup>
+      <FormGroup>
+        <FormInput
+          type="text"
+          autoComplete="off"
+          name="content"
+          value={input.content}
+          onChange={(e) => onChangeHandler(e)}
+        />
+        <HighLight />
+        <InputBar />
+        <Label>내용임</Label>
+      </FormGroup>
+      <FormGroup>
+        <FormInput
+          type="file"
+          id="file"
+          multiple="multiple"
+        />
+        <HighLight />
+        <InputBar />
+        <Label>사진첨부</Label>
+      </FormGroup>
 
-        <button>등록하기</button>
+      <button>등록하기</button>
     </FormContainer>
-  )
+  );
 };
