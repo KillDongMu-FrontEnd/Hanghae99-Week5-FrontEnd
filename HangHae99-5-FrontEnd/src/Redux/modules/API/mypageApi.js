ffedce6fc71462fc96469101eeb5ea3c4d70817f
Limsohee <v_vsoi@naver.com>
2022-10-27 06:59:03 +0900

    마이페이지 api정보 잘들어왔는지 값 확인

--- HangHae99-5-FrontEnd/src/Redux/modules/API/mypageApi.js
+++ HangHae99-5-FrontEnd/src/Redux/modules/API/mypageApi.js
@@ -4,7 +4,6 @@ const BASE_URL = process.env.REACT_APP_SERVER;
 const accessToken = localStorage.getItem("authorization");
 
 export const getMyPageIdApi = async (username) => {
-    console.log(username)
     const response = await axios.get(`${BASE_URL}/api/mypage/${username}`,{
       headers:{
         Authorization: accessToken,
